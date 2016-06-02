2968016e7621ca674765edd593a8cb64719a6c93
mathieudevos <mathieu.dvs@gmail.com>
2016-06-02 16:29:13 +0300

    logger complete

--- mainserver/logs/log.js
+++ mainserver/logs/log.js
@@ -1,1 +1,30 @@
 ROOT = process.cwd();
+var config = require(ROOT + '/config.json');
+console.log('Logs are going to: ' +  ROOT + config.log_path);
+
+var winston = require('winston');
+
+var logger = new(winston.Logger)({
+	transports: [
+	new(winston.transports.Console)({
+		json: false,
+		timestamp: true
+	}),
+	new winston.transports.File({
+		filename: ROOT + config.log_path + 'debug.log',
+		json: false
+	})
+	],
+	exceptionHandlers: [
+		new(winston.transports.Console)({
+			json: false,
+			timestamp: true
+		}),
+		new winston.transports.File({
+		filename: ROOT + config.log_path + 'debug.log',
+		json: false
+	})],
+		exitOnError: false
+});
+
+module.exports = logger;
